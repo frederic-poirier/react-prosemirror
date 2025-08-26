b572625f07674ef506d9387e7520a0595f612017
freddi <fredmpoirier@gmail.comgit config --global user.email fredmpoirier@gmail.comgit config --global user.email you@example.com  git config --global user.name Your>
2025-08-25 21:32:47 -0400

    fix: Correct casing in import path for AbbreviationComponent in PluginsSettings

--- src/features/settings/PluginsSettings.jsx
+++ src/features/settings/PluginsSettings.jsx
@@ -1,9 +1,9 @@
-import AbbreviationComponent from "../../editor/plugins/abbreviation/settings/AbbreviationComponent.jsx"
+import AbbreviationComponent from "../../Editor/plugins/abbreviation/settings/AbbreviationComponent.jsx"
 
 export default function PluginsSettings() {
     return (
         <>
-        <h1>Abbreviation</h1>
+            <h1>Abbreviation</h1>
             <AbbreviationComponent />
         </>
     )
